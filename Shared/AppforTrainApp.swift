c07de5fa50c3385ec9dba79baa51f8b36e6c506a
Jodi Pomalingo <44552031+Jipodevops@users.noreply.github.com>
2022-06-22 14:11:28 +0800

    Initial Commit

---
+++ Shared/AppforTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppforTrainApp.swift
+//  Shared
+//
+//  Created by J Pomalingo on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AppforTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
